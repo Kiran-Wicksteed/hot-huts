d7e7db0b820d629278532d8a85e07f1899b4614d
ClayNorwitz <clay@electricsheepagency.co.za>
2025-06-13 15:22:59 +0200

    updates

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,18 +1,5 @@
-import ApplicationLogo from "@/Components/ApplicationLogo";
-import { Link } from "@inertiajs/react";
+import styles from "../../styles";
 
 export default function GuestLayout({ children }) {
-    return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="h-8 w-auto fill-current text-gray-500" />
-                </Link>
-            </div>
-
-            <div className="mt-20 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
-                {children}
-            </div>
-        </div>
-    );
+    return <div className="grid grid-cols-3 h-screen">{children}</div>;
 }
